4a5ed16d37400c07f7e02b8ac8281325df6b9cdc
xisain <husenabs232bgr@gmail.com>
2025-10-27 18:01:52 +0700

    Fix : Show Untuk Private Course

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use SweetAlert2\Laravel\Swal;
 
 class CourseController extends Controller
 {
@@ -182,8 +183,20 @@ public function show($slug)
         ->with(['material.submaterial'])
         ->first();
 
+    // Course bukan public dan bukan admin
+    if(!$course->public && auth()->user()->role->id != 1) {
+        Swal::error([
+            'title' => 'Error',
+            'text'=> 'Kelas Tidak Di temukan'
+        ]);
+        return redirect()->route('course.index');
+    }
     if (!$course) {
-        abort(404, 'Kursus tidak ditemukan');
+         Swal::error([
+            'title' => 'Error',
+            'text'=> 'Kelas Tidak Di temukan'
+        ]);
+         return redirect()->route('course.index');
     }
 
     Log::info('Course ditemukan:', ['id' => $course->id, 'slug' => $slug]);
@@ -361,7 +374,6 @@ public function show($slug)
             // 3. Update materials in chunks
             $existingMaterialIds = $course->material()->pluck('id')->toArray();
             $updatedMaterialIds = [];
-
             foreach ($validated['materials'] as $materialIndex => $mat) {
                 DB::beginTransaction();
 
